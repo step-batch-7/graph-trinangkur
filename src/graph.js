d49e2861a490b33c489c6cbb4e2f16e2da68c3c7
Trinangkur Chatterjee <58026183+trinangkur@users.noreply.github.com>
2020-06-11 13:18:17 +0530

    implemented bfs
    for a given set of pairs it will retrun true if it has any path between
    source and target

--- src/graph.js
+++ src/graph.js
@@ -1,11 +1,44 @@
-//Example 
-// Pairs => [[from,to],[to,from]]
-// Source => from
-// To => to 
-// Should return true.
-
-const bfs = function(pairs,source,target){
-  
+﻿const parsePairs = function (pairs) {
+  const graph = {};
+  pairs.forEach((pair) => {
+    const parent = pair[0];
+    if (graph[parent] === undefined) {
+      graph[parent] = [];
+    }
+    graph[parent].push(pair[1]);
+  });
+  return graph;
 };
 
-module.exports = {bfs};
+const getChildren = (graph, parent) =>
+  graph[parent] === undefined ? [] : graph[parent];
+
+const updateVisited = (visited, newElements) => {
+  newElements.forEach((element) => {
+    visited.add(element);
+  });
+};
+
+const filterNonVisited = (graph, parent, visited) => {
+  const children = getChildren(graph, parent);
+  return children.filter((element) => !visited.has(element));
+};
+
+const bfs = function (pairs, source, target) {
+  const graph = parsePairs(pairs);
+  let queue = getChildren(graph, source);
+  const visited = new Set();
+  updateVisited(visited, queue);
+  while (queue.length !== 0) {
+    const currentElement = queue.shift();
+    if (currentElement == target) {
+      return true;
+    }
+    const nonVisited = filterNonVisited(graph, currentElement, visited);
+    queue = queue.concat(nonVisited);
+    updateVisited(visited, nonVisited);
+  }
+  return false;
+};
+
+module.exports = { bfs };
